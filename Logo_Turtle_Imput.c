3ad7edbe7c23a926280d3aa49d334259982d2fec
joardam <pedrofdias@live.com>
2023-08-08 14:22:02 -0300

    Terminei de modularizar internamente

--- Logo_Turtle_Imput.c
+++ Logo_Turtle_Imput.c
@@ -1,6 +1,38 @@
 #include <curses.h>
 #include <string.h>
 #include "Logo_Turtle.h"
+void turtleinit(int *sh, int *sw , WINDOW** w, WINDOW** prompt,int *turtle_y ,int *turtle_x){
+    initscr();
+    curs_set(1);
+    nodelay(stdscr, 1);
+    
+    getmaxyx(stdscr, *sh, *sw);
+    *w = newwin(*sh - 3, *sw, 0, 0);
+    box(*w, 0, 0);
+    wrefresh(*w);
+
+    // Window de comando
+    *prompt = newwin(3, *sw, *sh - 3, 0);
+    box(*prompt, 0, 0);
+    mvwprintw(*prompt, 1, 1, " Comandos: ");
+    wrefresh(*prompt);
+
+    // Turtle no centro
+    *turtle_y = (*sh - 3) / 2;
+    *turtle_x = *sw / 2;
+    mvwaddch(*w, *turtle_y, *turtle_x, '@');
+    wrefresh(*w);
+
+}
+
+void setprompt(WINDOW** prompt, char command[]){
+    wmove(*prompt, 1, 11);
+    wclrtoeol(*prompt); // limpa o input
+    wrefresh(*prompt);
+    echo();
+    wgetnstr(*prompt, command, 100);
+    noecho();
+}
 
 int checkinput(const char* sArray[] , const char cmd[]){
 
@@ -32,38 +64,37 @@ void definetrace(char *traceback , const char cmd[]){
         }
 }
 
-
-void moveturtle(const char cmd[] , int *new_turtle_y , int *new_turtle_x , int pen_down , int value , char traceback , WINDOW* w){
+void moveturtle(const char cmd[] , int *turtle_y , int *turtle_x , int pen_down , int value , char traceback , WINDOW* w){
     if (strcmp(cmd, "up") == 0) {
         for (int i = 0; i < value; i++) {
-            *new_turtle_y -= 1;
+            *turtle_y -= 1;
             if (pen_down) {
-                mvwaddch(w, *new_turtle_y, *new_turtle_x, traceback);
+                mvwaddch(w, *turtle_y, *turtle_x, traceback);
             }
         }  
     }
     else if (strcmp(cmd, "dw") == 0) {
         //down
         for (int i = 0; i < value; i++) {
-            *new_turtle_y += 1;
+            *turtle_y += 1;
             if (pen_down) {
-                mvwaddch(w, *new_turtle_y, *new_turtle_x, traceback);
+                mvwaddch(w, *turtle_y, *turtle_x, traceback);
             }
         }
     }
     else if (strcmp(cmd, "lt") == 0) {//left
         for (int i = 0; i < value; i++) {
-            *new_turtle_x -= 1;
+            *turtle_x -= 1;
             if (pen_down) {
-                mvwaddch(w, *new_turtle_y, *new_turtle_x, traceback);
+                mvwaddch(w, *turtle_y, *turtle_x, traceback);
             }
         }
     }
     else if (strcmp(cmd, "rt") == 0) {//rigth
         for (int i = 0; i < value; i++) {
-            *new_turtle_x += 1;
+            *turtle_x += 1;
             if (pen_down) {
-                mvwaddch(w, *new_turtle_y, *new_turtle_x, traceback);
+                mvwaddch(w, *turtle_y, *turtle_x, traceback);
             }
         }
     }
@@ -71,201 +102,188 @@ void moveturtle(const char cmd[] , int *new_turtle_y , int *new_turtle_x , int p
 
 }
 
-void drawfigure(const char cmd[] , int *new_turtle_y , int *new_turtle_x , int pen_down , int value , WINDOW* w){
+void drawfigure(const char cmd[] , int *turtle_y , int *turtle_x , int pen_down , int value , WINDOW* w){
     if (strcmp(cmd, "sq") == 0) {//Quadrado
         for (int i = 0; i < 12; i++) {
-            *new_turtle_x += 1;
-            mvwaddch(w, *new_turtle_y, *new_turtle_x, '-');
+            *turtle_x += 1;
+            mvwaddch(w, *turtle_y, *turtle_x, '-');
         }
         for (int i = 0; i < 6; i++) {
-            *new_turtle_y += 1;
-            mvwaddch(w, *new_turtle_y, *new_turtle_x, '|');
+            *turtle_y += 1;
+            mvwaddch(w, *turtle_y, *turtle_x, '|');
         }
         for (int i = 0; i < 12; i++) {
-            *new_turtle_x -= 1;
-            mvwaddch(w, *new_turtle_y, *new_turtle_x, '-');
+            *turtle_x -= 1;
+            mvwaddch(w, *turtle_y, *turtle_x, '-');
         }
         for (int i = 0; i < 6; i++) {
-            *new_turtle_y -= 1;
-            mvwaddch(w, *new_turtle_y, *new_turtle_x, '|');
+            *turtle_y -= 1;
+            mvwaddch(w, *turtle_y, *turtle_x, '|');
         }
     }
     else if (strcmp(cmd, "tg") == 0) {//Triangulo
         for (int i = 0; i < 6; i++) {
-            *new_turtle_y -= 1;
-            *new_turtle_x += 1;
-            mvwaddch(w, *new_turtle_y, *new_turtle_x, '/');
+            *turtle_y -= 1;
+            *turtle_x += 1;
+            mvwaddch(w, *turtle_y, *turtle_x, '/');
         }
         for (int i = 0; i < 7; i++) {
-            *new_turtle_y += 1;
-            *new_turtle_x += 1;
-            mvwaddch(w, *new_turtle_y, *new_turtle_x, '\\');
+            *turtle_y += 1;
+            *turtle_x += 1;
+            mvwaddch(w, *turtle_y, *turtle_x, '\\');
         }
         for (int i = 0; i < 14; i++) {
-            *new_turtle_x -= 1;
-            mvwaddch(w, *new_turtle_y, *new_turtle_x, '-');
+            *turtle_x -= 1;
+            mvwaddch(w, *turtle_y, *turtle_x, '-');
         }
     }
     else if (strcmp(cmd, "dm") == 0) {
         for (int i = 0; i < 6; i++) {
-            *new_turtle_y -= 1;
-            *new_turtle_x += 1;
-            mvwaddch(w, *new_turtle_y, *new_turtle_x, '/');
+            *turtle_y -= 1;
+            *turtle_x += 1;
+            mvwaddch(w, *turtle_y, *turtle_x, '/');
         }
         for (int i = 0; i < 6; i++) {
-            *new_turtle_y += 1;
-            *new_turtle_x += 1;
-            mvwaddch(w, *new_turtle_y, *new_turtle_x, '\\');
+            *turtle_y += 1;
+            *turtle_x += 1;
+            mvwaddch(w, *turtle_y, *turtle_x, '\\');
         }
         for (int i = 0; i < 6; i++) {
-            *new_turtle_y += 1;
-            *new_turtle_x -= 1;
-            mvwaddch(w, *new_turtle_y, *new_turtle_x, '/');
+            *turtle_y += 1;
+            *turtle_x -= 1;
+            mvwaddch(w, *turtle_y, *turtle_x, '/');
         }
         for (int i = 0; i < 6; i++) {
-            *new_turtle_y -= 1;
-            *new_turtle_x -= 1;
-            mvwaddch(w, *new_turtle_y, *new_turtle_x, '\\');
+            *turtle_y -= 1;
+            *turtle_x -= 1;
+            mvwaddch(w, *turtle_y, *turtle_x, '\\');
         }
     }
     else if (strcmp(cmd, "ci") == 0) {
         for (int i = 0; i < 7; i++) {
-            *new_turtle_x += 1;
-            mvwaddch(w, *new_turtle_y, *new_turtle_x, '-');
+            *turtle_x += 1;
+            mvwaddch(w, *turtle_y, *turtle_x, '-');
         }
         for (int i = 0; i < 2; i++) {
-            *new_turtle_y += 1;
-            *new_turtle_x += 1;
-            mvwaddch(w, *new_turtle_y, *new_turtle_x, '\\');
+            *turtle_y += 1;
+            *turtle_x += 1;
+            mvwaddch(w, *turtle_y, *turtle_x, '\\');
         }
         for (int i = 0; i < 2; i++) {
-            *new_turtle_y += 1;
-            mvwaddch(w, *new_turtle_y, *new_turtle_x, '|');
+            *turtle_y += 1;
+            mvwaddch(w, *turtle_y, *turtle_x, '|');
         }
         for (int i = 0; i < 2; i++) {
-            *new_turtle_y += 1;
-            *new_turtle_x -= 1;
-            mvwaddch(w, *new_turtle_y, *new_turtle_x, '/');
+            *turtle_y += 1;
+            *turtle_x -= 1;
+            mvwaddch(w, *turtle_y, *turtle_x, '/');
         }
         for (int i = 0; i < 7; i++) {
-            *new_turtle_x -= 1;
-            mvwaddch(w, *new_turtle_y, *new_turtle_x, '-');
+            *turtle_x -= 1;
+            mvwaddch(w, *turtle_y, *turtle_x, '-');
         }
         for (int i = 0; i < 2; i++) {
-            *new_turtle_y -= 1;
-            *new_turtle_x -= 1;
-            mvwaddch(w, *new_turtle_y, *new_turtle_x, '\\');
+            *turtle_y -= 1;
+            *turtle_x -= 1;
+            mvwaddch(w, *turtle_y, *turtle_x, '\\');
         }
         for (int i = 0; i < 2; i++) {
-            *new_turtle_y -= 1;
-            mvwaddch(w, *new_turtle_y, *new_turtle_x, '|');
+            *turtle_y -= 1;
+            mvwaddch(w, *turtle_y, *turtle_x, '|');
         }
         for (int i = 0; i < 2; i++) {
-            *new_turtle_y -= 1;
-            *new_turtle_x += 1;
-            mvwaddch(w, *new_turtle_y, *new_turtle_x, '/');
+            *turtle_y -= 1;
+            *turtle_x += 1;
+            mvwaddch(w, *turtle_y, *turtle_x, '/');
         }
     }
 }
 
+void doclear(const char cmd[] , int *turtle_y , int *turtle_x ,const int sh , const int sw , WINDOW* w) {
+    wclear(w);
+    box(w, 0, 0);
+    *turtle_x = sw / 2;
+    *turtle_y = (sh - 3) / 2;
+    mvwaddch(w, *turtle_y, *turtle_x, '@');
+    
+}
+
+void movlim(int * turtle_y , int * turtle_x , const int sh , const int sw){
+    if (*turtle_y < 0) *turtle_y = 0;
+    if (*turtle_y >= sh - 3) *turtle_y = sh - 4;
+    if (*turtle_x < 0) *turtle_x = 0;
+    if (*turtle_x >= sw) *turtle_x = sw - 1;
+}
+
+
+//Comandos cmd
+const char *possibleCommands[] = {
+    "up", "dw", "lt", "rt" , 
+    "pc", "cl", "sq", "tg" ,
+    "dm", "ci", "ex", NULL
+};
+
+const char *zNuComands[] = { 
+    "pc", "cl", "sq", "tg" ,
+    "dm", "ci", "ex", NULL
+};
+
+const char * cmdmoveturtle[] = {
+    "up", "dw", "lt", "rt" , NULL
+};
+
+const char * cmddrawfigure[] = {
+    "sq" , "tg" , "dm" , "ci" , NULL
+};
+
+
 int main() {
     WINDOW* w, *prompt;
-
-    initscr();
-    curs_set(1);
-    nodelay(stdscr, 1);
-
+    
+    int turtle_y ; int turtle_x ;
     int sh, sw;
-    getmaxyx(stdscr, sh, sw);
-    w = newwin(sh - 3, sw, 0, 0);
-    box(w, 0, 0);
-    wrefresh(w);
-
-    // Window de comando
-    prompt = newwin(3, sw, sh - 3, 0);
-    box(prompt, 0, 0);
-    mvwprintw(prompt, 1, 1, " Comandos: ");
-    wrefresh(prompt);
-
-    // Turtle no centro
-    int turtle_y = (sh - 3) / 2;
-    int turtle_x = sw / 2;
-    mvwaddch(w, turtle_y, turtle_x, '@');
-    wrefresh(w);
+    turtleinit(&sh , &sw , &w , &prompt, &turtle_y , &turtle_x);
+    
 
     // Pen down
     int pen_down = 1;
     char command[100];
 
     while (1) {
-        wmove(prompt, 1, 11);
-        wclrtoeol(prompt); // limpa o input
-        wrefresh(prompt);
-        echo();
-        wgetnstr(prompt, command, sizeof(command));
-        noecho();
-
+        setprompt(&prompt,command);
         if (!command[0]) {
             continue;
         }
         else {
             char cmd[10];
             int value;
-   
-            const char *possibleCommands[] = {
-                "up", "dw", "lt", "rt" , 
-                "pu", "cl", "sq", "tg" ,
-                "dm", "ci", "ex", NULL
-            };
-
-            const char *zNuComands[] = {
-                "up", "dw", "lt", "rt" , 
-                "pu", "cl", "sq", "tg" ,
-                "dm", "ci", "ex", NULL
-            };
 
             int checks = sscanf(command, "%s %d", cmd, &value);
+            
             if ((checks != 2 || !checkinput(possibleCommands,cmd)) & !(checks >= 1 & checkinput(zNuComands, cmd))) {
                 continue;
             }
 
             // Comandos
-            int new_turtle_y = turtle_y;
-            int new_turtle_x = turtle_x;
-
             char traceback;
             definetrace(&traceback, cmd);
 
             // Apaga a posi��o atual
-            if (!(strcmp(cmd, "pu") == 0)) {
+            if (!(strcmp(cmd, "pc") == 0)) {
                 mvwaddch(w, turtle_y, turtle_x, pen_down ? traceback : ' ');
             }
     
 
-            const char * cmdmoveturtle[] = {
-                "up", "dw", "lt", "rt" , NULL
-            };
-            
-            const char * cmddrawfigure[] = {
-                "sq" , "tg" , "dm" , "ci" , NULL
-            };
-
             if (checkinput(cmdmoveturtle, cmd)){
-                moveturtle(cmd , &new_turtle_y , &new_turtle_x , pen_down , value, traceback , w);
+                moveturtle(cmd , &turtle_y , &turtle_x , pen_down , value, traceback , w);
             }
-
             else if (checkinput(cmddrawfigure,cmd)){
-                drawfigure(cmd , &new_turtle_y , &new_turtle_x , pen_down , value, w);
+                drawfigure(cmd , &turtle_y , &turtle_x , pen_down , value, w);
             }
-            
             else if (strcmp(cmd, "cl") == 0) {
-                wclear(w);
-                box(w, 0, 0);
-                new_turtle_x = sw / 2;
-                new_turtle_y = (sh - 3) / 2;
-                mvwaddch(w, new_turtle_y, new_turtle_x, '@');
+                doclear(cmd , &turtle_y , &turtle_x , sh , sw , w);
             }
-            else if (strcmp(cmd, "pu") == 0) {
+            else if (strcmp(cmd, "pc") == 0) {
                 pen_down = !pen_down;
             }
             else if (strcmp(cmd, "ex") == 0) {
@@ -275,15 +293,10 @@ int main() {
                 continue;
             }
 
-            // Movimenta��o dentro da tela
-            if (new_turtle_y < 0) new_turtle_y = 0;
-            if (new_turtle_y >= sh - 3) new_turtle_y = sh - 4;
-            if (new_turtle_x < 0) new_turtle_x = 0;
-            if (new_turtle_x >= sw) new_turtle_x = sw - 1;
+            // limita movimentação
+            movlim(&turtle_y , &turtle_x , sh , sw);
 
             // Nova posi��o
-            turtle_y = new_turtle_y;
-            turtle_x = new_turtle_x;
             mvwaddch(w, turtle_y, turtle_x, '@');
 
             wrefresh(w);
